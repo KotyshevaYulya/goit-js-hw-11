7d628eb9fac27d37697088317b18a01546762196
KotyshevaYulya <92879736+KotyshevaYulya@users.noreply.github.com>
2024-03-30 18:44:41 +0100

    update

--- src/main.js
+++ src/main.js
@@ -1,1 +1,47 @@
-import * as myModule from './js/pixabay-api';
+"use strict";
+
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+//* = = = =
+
+import { getPhoto } from './js/pixabay-api'
+import { symbolTemplate } from './js/render-functions';
+
+export const form = document.querySelector('.search-form');
+export const gallery = document.querySelector('.gallery');
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const userSearch = form.elements.query.value;
+    if (userSearch === '') {
+         iziToast.error({
+        color: 'red',
+        position: 'topRight',
+        message: `Будь ласка введіть дані!`,
+      });
+    } else {
+        getPhoto(userSearch).then(data => {
+            if (data.total === 0) {
+                iziToast.error({
+                    color: 'red',
+                    position: 'topRight',
+                    message: `"Sorry, there are no images matching your search query. Please try again!"`,
+                });
+                 e.target.reset();
+            } else {
+                gallery.innerHTML = "";
+                const markup = symbolTemplate(data.hits);
+                gallery.insertAdjacentHTML("beforeend", markup);
+                e.target.reset();
+
+                const galleryBig = new SimpleLightbox('.gallery a', {
+                    captionsData: 'alt',
+                    captionDelay: 250,
+                });
+                
+            }
+        });
+    }
+});
